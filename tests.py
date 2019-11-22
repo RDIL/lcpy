764f43b68c8f91ca20e21a60132a791d276634fc
Reece Dunham <me@rdil.rocks>
2019-11-22 14:24:30 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import unittest
 try:
-    from lcpy import none, true, false
+    from lcpy import none, true, false, exception
 except ImportError:
     raise OSError("Failed to import the library.")
 
@@ -8,10 +8,14 @@ except ImportError:
 class Tests(unittest.TestCase):
     def test_values(self):
         self.assertEqual(none, None)
-        
+
     def test_bools(self):
         self.assertTrue(true, True)
         self.assertFalse(false, False)
+
+    def test_exception(self):
+        with self.assertRaises(Exception):
+            raise exception("This should be properly thrown!")
 
 
 if __name__ == '__main__':
