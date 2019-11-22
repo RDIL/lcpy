6be17a56845dff67d7ceffa315c6f156e3817852
Reece Dunham <me@rdil.rocks>
2019-11-22 14:20:42 -0500

    Adding alias for exception

--- lcpy/__init__.py
+++ lcpy/__init__.py
@@ -1,7 +1,10 @@
-#  Copyright (c) 2019-present RDIL.
-#  You should have received a copy of the
-#  MIT License with this program/distribution.
+"""
+Copyright (c) 2019-present Reece Dunham.
+You should have received a copy of the
+MIT License with this program/distribution.
+"""
 
 true = True
 false = False
 none = None
+exception = Exception
